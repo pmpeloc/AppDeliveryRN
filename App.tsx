6a81c61fbd359cc4d8d53e9058983bf50f95f8b1
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-28 15:40:37 -0300

    feat: Design App Screen Part-3 added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,12 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  View,
+} from 'react-native';
 
 export default function App() {
   return (
@@ -11,7 +19,42 @@ export default function App() {
         <Image style={styles.logoImage} source={require('./assets/logo.png')} />
         <Text style={styles.logoText}>FOOD APP</Text>
       </View>
-      <View style={styles.form}></View>
+      <View style={styles.form}>
+        <Text style={styles.formText}>INGRESAR</Text>
+        <View style={styles.formInput}>
+          <Image
+            style={styles.formIcon}
+            source={require('./assets/email.png')}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Correo electrónico'
+            keyboardType='email-address'
+          />
+        </View>
+        <View style={styles.formInput}>
+          <Image
+            style={styles.formIcon}
+            source={require('./assets/password.png')}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Contraseña'
+            keyboardType='default'
+            secureTextEntry
+          />
+        </View>
+        <View style={{ marginTop: 30 }}>
+          <Button
+            title='ENTRAR'
+            onPress={() => ToastAndroid.show('CLICK', ToastAndroid.LONG)}
+          />
+        </View>
+        <View style={styles.formRegister}>
+          <Text>No tienes cuenta?</Text>
+          <Text style={styles.formRegisterText}>Registrate</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -35,6 +78,39 @@ const styles = StyleSheet.create({
     bottom: 0,
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
+    padding: 30,
+  },
+  formInput: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  formIcon: {
+    width: 25,
+    height: 25,
+    marginTop: 5,
+  },
+  formText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  formTextInput: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#aaa',
+    marginLeft: 15,
+  },
+  formRegister: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  formRegisterText: {
+    fontStyle: 'italic',
+    color: 'orange',
+    borderBottomWidth: 1,
+    borderBottomColor: 'orange',
+    fontWeight: 'bold',
+    marginLeft: 10,
   },
   logoContainer: {
     position: 'absolute',
